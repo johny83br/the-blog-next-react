2bf07b4fa7c01c3bfea41c764d5c8be855c18ec5
Jônata Marcelino <jonata.marcelino83@gmail.com>
2025-12-09 17:31:44 -0300

    Criado template de título da metadata

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'About Page',
+  description: 'Teste',
+};
+
+export default function AboutPage() {
+  return <h1>About</h1>;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { Container } from '@/components/Container';
+import { Header } from '@/components/Header';
 
 export const metadata: Metadata = {
-  title: 'The Blog - Este é um blog com Next.js',
+  title: {
+    default: 'Este é um blog com Next.js | The Blog',
+    template: '%s | The Blog',
+  },
   description: 'Essa seria a descrição da página.',
 };
 
@@ -11,7 +16,17 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang='pt-BR' className='dark'>
-      <body>{children}</body>
+      <body>
+        <Container>
+          <Header />
+
+          {children}
+
+          <footer>
+            <h1 className='text-6xl font-bold text-center py-8'>FOOTER</h1>
+          </footer>
+        </Container>
+      </body>
     </html>
   );
 }
