af936bc58562581f92736a3e0072b3760dea6f27
Jônata Marcelino <jonata.marcelino83@gmail.com>
2026-01-13 02:11:30 -0300

    Estilizando o menu do admin com Tailwind CSS

--- src/components/Admin/Menu/index.tsx
+++ src/components/Admin/Menu/index.tsx
@@ -1,13 +1,84 @@
+import clsx from 'clsx';
+import { FileTextIcon, HouseIcon } from 'lucide-react';
 import Link from 'next/link';
 
 export function MenuAdmin() {
+  const navClasses = clsx(
+    'bg-slate-900 text-slate-100 rounded-lg mb-8',
+    'flex flex-col',
+    'sm:flex-row sm:flex-wrap',
+    // 'h-10',
+    'overflow-hidden',
+  );
+  const linkClasses = clsx(
+    '[&>svg]:w-4 [&>svg]:h-4 px-4',
+    'flex items-center justify-start gap-2',
+    'transition hover:bg-slate-800',
+    'h-10',
+    'shrink-0',
+  );
+
   return (
-    <nav>
-      <a href='/' target='_blank'>
+    <nav className={navClasses}>
+      <a className={linkClasses} href='/' target='_blank'>
+        <HouseIcon />
         Home
       </a>
 
-      <Link href='/admin/posts'>Posts</Link>
+      <Link className={linkClasses} href='/admin/posts'>
+        <FileTextIcon />
+        Posts
+      </Link>
+
+      <Link className={linkClasses} href='/admin/posts'>
+        <FileTextIcon />
+        Posts
+      </Link>
+
+      <Link className={linkClasses} href='/admin/posts'>
+        <FileTextIcon />
+        Posts
+      </Link>
+
+      <Link className={linkClasses} href='/admin/posts'>
+        <FileTextIcon />
+        Posts
+      </Link>
+
+      <Link className={linkClasses} href='/admin/posts'>
+        <FileTextIcon />
+        Posts
+      </Link>
+
+      <Link className={linkClasses} href='/admin/posts'>
+        <FileTextIcon />
+        Posts
+      </Link>
+
+      <Link className={linkClasses} href='/admin/posts'>
+        <FileTextIcon />
+        Posts
+      </Link>
+
+      <Link className={linkClasses} href='/admin/posts'>
+        <FileTextIcon />
+        Posts
+      </Link>
+
+      <Link className={linkClasses} href='/admin/posts'>
+        <FileTextIcon />
+        Posts
+      </Link>
+
+      <Link className={linkClasses} href='/admin/posts'>
+        <FileTextIcon />
+        Posts
+      </Link>
+
+      <Link className={linkClasses} href='/admin/posts'>
+        <FileTextIcon />
+        Posts
+      </Link>
     </nav>
   );
 }
