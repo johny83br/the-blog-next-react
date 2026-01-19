c764f1ef6d1eab97166a79b1af7500f3296dfc2e
Jônata Marcelino <jonata.marcelino83@gmail.com>
2026-01-19 15:23:59 -0300

    Criado o componente do Markdown com @uiw/react-md-editor

--- src/app/admin/posts/new/page.tsx
+++ src/app/admin/posts/new/page.tsx
@@ -1,38 +1,12 @@
-import { InputCheckbox } from '@/components/Forms/InputCheckbox';
-import { InputText } from '@/components/Forms/InputText';
+import { ManagePostForm } from '@/components/Admin/ManagePostForm';
 
 export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
   return (
-    <div className='flex flex-col gap-6 mb-8'>
-      <InputText labelText='Nome' placeholder='Digite seu nome' />
-      <InputText labelText='Sobrenome' placeholder='Digite seu sobrenome' />
-
-      <InputCheckbox labelText='Aceito os termos e condições' />
-
-      <InputText
-        disabled
-        labelText='Sobrenome'
-        placeholder='Digite seu sobrenome'
-      />
-      <InputText
-        disabled
-        value='Olá mundo!'
-        labelText='Sobrenome'
-        placeholder='Digite seu sobrenome'
-      />
-      <InputText
-        readOnly
-        labelText='Sobrenome'
-        placeholder='Digite seu sobrenome'
-      />
-      <InputText
-        readOnly
-        value='Olá mundo!'
-        labelText='Sobrenome'
-        placeholder='Digite seu sobrenome'
-      />
-    </div>
+    <>
+      <h1>Criar post</h1>
+      <ManagePostForm />
+    </>
   );
 }
---
+++ src/components/Admin/ManagePostForm/index.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { Button } from '@/components/Forms/Button';
+import { InputCheckbox } from '@/components/Forms/InputCheckbox';
+import { InputText } from '@/components/Forms/InputText';
+import { MarkdownEditor } from '@/components/Forms/MarkdownEditor';
+import React from 'react';
+
+export const dynamic = 'force-dynamic';
+
+export function ManagePostForm() {
+  const [content, setContent] = React.useState('');
+
+  return (
+    <form method='' className='mb-16'>
+      <div className='flex flex-col gap-6'>
+        <InputText labelText='Título' placeholder='Digite o título do post' />
+
+        <MarkdownEditor
+          labelText='Conteúdo'
+          disabled={false}
+          textAreaName='content'
+          value={content}
+          setValue={setContent}
+        />
+
+        <InputCheckbox labelText='Aceito os termos e condições' />
+
+        <div className='mt-8'>
+          <Button type='submit'>Enviar</Button>
+        </div>
+      </div>
+    </form>
+  );
+}
